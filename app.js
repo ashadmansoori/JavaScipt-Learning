692271596e5a0acdf041f03fa796fe507e5eb6cd
ashad mansoori <mansooriashad40@gmail.com>
2023-02-06 13:24:05 +0500

    first class code

---
+++ app.js
@@ -0,0 +1,57 @@
+
+
+// -------------------------- Class-01 -------------------------------------
+
+
+//Data Types
+
+// String = "ashad 123"
+// Number = 123
+// Boolean = true || false
+
+
+//console
+
+// console.log("ashad" +" "+ "mansuri")
+// console.log(5 + 10);
+// console.log("5" + "10");
+
+
+//Variables
+
+// var ashad = "mansoori";
+// console.log(ashad);
+
+// var num1 = 5;
+// var num2 = 10;
+// console.log(num1 + num2);
+
+
+// alert & prompt
+
+// alert("ashad");
+// var username = prompt("What is your name?" , "ashad");
+// console.log(username);
+
+
+//calculations
+
+// var mathMarks = +prompt("Enter your maths marks");
+// var chemMarks = +prompt("Enter your chemistry marks");
+// var physMarks = +prompt("Enter your physics marks");
+// var islMarks = +prompt("Enter your islamiat marks");
+
+// var userMarks = mathMarks + chemMarks + physMarks + islMarks;
+// var totalMarks = 400;
+// var percentage = userMarks / totalMarks * 100;
+// console.log("you got " + percentage + "%");
+
+
+
+
+
+
+
+// -------------------------- Class-02 -------------------------------------
+
+
