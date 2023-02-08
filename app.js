499e014d1352e913340f5da2d5ff22f63a1af78d
ashad mansoori <mansooriashad40@gmail.com>
2023-02-08 15:40:07 +0500

    class-02 done

--- app.js
+++ app.js
@@ -58,3 +58,97 @@
 // -------------------------- Class-02 -------------------------------------
 
 
+
+//Functions
+
+// function abc(){
+//     console.log("ashad");
+// }
+// abc()
+
+
+
+// function names(name , email , password) {
+//     console.log(name);
+//     console.log(email);
+//     console.log(password);
+// }
+
+// names("abdullah" , "m@gmail.com" , "5156151");
+
+
+
+// function qasim (dish1 , dish2 , guest){
+//     console.log("Qasim janu " + guest + " ay hain " + dish1 + " or " + dish2 + " la ao" );
+// }
+
+// qasim("tindaa" , "roti" , "ashad");
+
+
+// var button = document.getElementById("btn");
+// // console.log(button);
+
+// function callName(){
+//     console.log("Ashad");
+// }
+
+
+
+
+// var names = document.getElementById("name");
+// var email = document.getElementById("email");
+
+
+// function getVal(){
+//     console.log(names.value);
+//     console.log(email.value);
+// }
+
+
+
+
+
+// var pName = document.getElementById("para");
+
+
+// function getVal(){
+//     para.innerHTML = "ashad";
+// }
+
+
+
+// function liveInSindh() {
+//     pName.innerHTML = "You live in Sindh";
+// }
+
+// function liveInPunjab() {
+//     pName.innerHTML = "You live in Punjab";
+// }
+
+// function liveInKP() {
+//     pName.innerHTML = "You live in KP";
+// }
+
+// function liveInBalochistan() {
+//     pName.innerHTML = "You live in Balochistan";
+// }
+
+// function liveInCapital() {
+//     pName.innerHTML = "You live in Capital";
+// }
+
+
+
+
+
+
+// -------------------------- Class-03 -------------------------------------
+
+
+
+
+
+
+
+
+
